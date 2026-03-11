e129de682f6b746b1a0497e3c39ae8dbc0a722ca
Ben <bwebstercapo@gmail.com>
2026-03-11 15:30:53 -0700

    testing css

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var items = ["test1", "test2", "test3"];
 
+  // connects to the itemList element
   const itemList = document.getElementById('itemList');
 
   for (const item of items) {
+
+    // Creates the div
     const itemDiv = document.createElement('div');
+    // Gives it a css class
     itemDiv.className = 'listItem'
 
-    const nameParagraph = document.createElement('p');
-    nameParagraph.textContent = `Name: ${item}`
+    // Creates a checkbox
+    const itemCheckbox = document.createElement('input')
+    itemCheckbox.type = "checkbox"
 
+    // Creates the name text
+    const nameParagraph = document.createElement('p');
+    // Gives it the content
+    nameParagraph.textContent = `${item}`
+
+    // Addes them to the hierarchy
+    itemDiv.appendChild(itemCheckbox);
     itemDiv.appendChild(nameParagraph);
     itemList.appendChild(itemDiv);
   };
@@ -19,4 +31,5 @@ document.addEventListener('DOMContentLoaded', function() {
   function addItem(item) {
     items.addItem("item1")
   }
+
 });
