5e4b25479dfd4f025dcbfc618e8f907c2f52df7a
Ben <bwebstercapo@gmail.com>
2026-03-13 16:25:18 -0700

    multiple lists working now

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-// --- Supabase Setup ---
+// Supabase Setup -------------------------------------------------------------------
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 const supabaseUrl = 'https://musdgyvvxpeztkbiysvl.supabase.co'; // Replace with yours
@@ -11,14 +11,18 @@ console.log("Supabase is loaded and connected!");
 
 const loginSection = document.getElementById('loginSection');
 const mainSection = document.getElementById('mainSection');
+const listsSection = document.getElementById('listsSection');
 
 const emailInput = document.getElementById('emailInput');
 const passwordInput = document.getElementById('passwordInput');
 const loginButton = document.getElementById('loginButton');
 const loginMessage = document.getElementById('loginMessage');
 const logoutButton = document.getElementById('logoutButton');
+// -------------------------------------------------------------------
 
 
+
+// Persistent Login Session -------------------------------------------------------------------
 // Check current session to maintain login
 async function checkCurrentSession() {
   // Look for a saved session in the browser
@@ -29,8 +33,22 @@ async function checkCurrentSession() {
     mainSection.style.display = 'block';
     console.log("Welcome back:", session.user.email);
 
-    const data = await fetchItems();
-    createListUI(data);
+    const { data, error } = await supabase
+      .from("List")
+      .select();
+
+    // Clears existing content
+    // listsSection.innerHTML = '';
+
+    console.log("lists_data:", data);
+    for (const list_data of data) {
+      const list_id = list_data.list_id;
+      console.log("list_id:", list_id);
+      const data = await fetchItems(list_id);
+
+      createListUI(data);
+    }
+
 
   } else {
     // No saved session found.
@@ -39,7 +57,11 @@ async function checkCurrentSession() {
     console.log("No user logged in.");
   }
 }
+// -------------------------------------------------------------------
 
+
+
+// Login and Logout -------------------------------------------------------------------
 // Login Button Click
 loginButton.addEventListener('click', async () => {
   const email = emailInput.value;
@@ -58,7 +80,7 @@ loginButton.addEventListener('click', async () => {
     loginMessage.innerText = "";
     emailInput.value = "";
     passwordInput.value = "";
-    checkCurrentSession(); // Re-run the check to hide the login screen!
+    checkCurrentSession();
   }
 });
 
@@ -68,17 +90,22 @@ logoutButton.addEventListener('click', async () => {
   checkCurrentSession();
 });
 
-
 // Run right as page loads
 checkCurrentSession();
+// -------------------------------------------------------------------
 
 
+
+// -------------------------------------------------------------------
 // Logic to write data
 document.getElementById('writeDataButton').addEventListener('click', async () => {
+  writeItem("4f53413d-7171-45d2-b066-0eca0f7e7afd", "test_item_in_list", 1);
+});
 
+async function writeItem(list_id = null, name, qty) {
   const newItem = {
-    item_name: "test_item",
-    item_qty: 0
+    item_name: name,
+    item_qty: qty
   };
 
   const { data, error } = await supabase
@@ -92,69 +119,141 @@ document.getElementById('writeDataButton').addEventListener('click', async () =>
     console.log("Successfully added:", data);
   }
 
-});
-
-
-// Logic to read data
-// fetches all items
-async function readItems() {
-  const { data, error } = await supabase
-    .from("Item")
-    .select()
-
-  for (const item of data) {
-    console.log(item.item_name)
+  if (list_id) {
+    await addItemToList(list_id, data[0]);
   }
+
+  checkCurrentSession();
+}
+
+
+async function addItemToList(list_id, newItemData) {
+
+  const newListItem = {
+    item_id: newItemData.item_id,
+    list_id: list_id
+  }
+
+  const { data, error } = await supabase
+    .from('ListItem')
+    .insert([newListItem])
+    .select();
 
   if (error) {
-    console.error("Error fetching data:", error.message);
+    console.error("Error adding item to list:", error.message);
   } else {
-    console.log("Successfully fetched:", data);
+    console.log("Successfully added item to list:", data);
   }
-  return data;
+}
+// -------------------------------------------------------------------
+
+
+
+// -------------------------------------------------------------------
+// Logic to read data
+// fetches all items
+async function readItems(list_id = null) {
+
+  console.log("reading items!");
+  if (list_id != null) {
+    //
+    // implement lists-specific stuff here!!!!
+    //
+    console.log(`Fetching from ${list_id}`)
+    const { data, error } = await supabase
+      .from('List')
+      .select
+      (`
+        list_id,
+        list_name,
+        ListItem 
+          (
+          Item 
+            (
+            item_id, 
+            item_name, 
+            is_checked, 
+            item_qty
+          )
+        )
+      `)
+      .eq('list_id', list_id); // Basically a SQL 'WHERE' clause
+    console.log(data[0])
+    console.log(`${data[0].list_name}`);
+    console.log(`${data[0]["ListItem"][0]}`);
+
+    return data
+  }
+
+  else {
+
+
+    const { data, error } = await supabase
+      .from("Item")
+      .select();
+
+    for (const item of data) {
+      console.log(item.item_name)
+    }
+
+    if (error) {
+      console.error("Error fetching data:", error.message);
+    } else {
+      console.log("Successfully fetched:", data);
+    }
+    return data;
+  }
 }
 
-async function fetchItems() {
-  return readItems();
+
+async function fetchItems(list_id = null) {
+  return readItems(list_id);
 }
-
-
-// Account Creation
-// async function signUpUser() {
-//   const { data, error } = await supabase.auth.signUp({
-//     email: 'bkwebster@willamette.edu',
-//     password: 'Fefe1234Alameda67!'
-//   });
-//
-//   if (error) {
-//     console.error("Sign up error:", error.message);
-//   } else {
-//     console.log("Account created! Check your email to verify (if enabled in Supabase settings).");
-//   }
-// }
+// -------------------------------------------------------------------
 
 
 
-
+// -------------------------------------------------------------------
 function createListUI(data) {
+
+  console.log(data);
   // connects to the itemList element
-  const itemListElement = document.getElementById('itemList');
 
-  // Clears existing content
-  itemListElement.innerHTML = '';
+  const itemListElement = document.createElement('div')
 
-  for (const item of data) {
+  listsSection.appendChild(itemListElement);
+
+  // Extract components of data
+  const list_data = data[0];
+  const items_data = data[0]["ListItem"];
+  console.log("list_data: ", list_data);
+  console.log("list_data.list_name: ", list_data.list_name);
+
+  // TODO :
+  // Creates list header
+  const listHeader = document.createElement('h2');
+  const list_name = list_data.list_name;
+  const list_id = list_data.list_id;
+  listHeader.textContent = list_name;
+  listHeader.className = "listHeader";
+  itemListElement.appendChild(listHeader);
+
+  for (const item_data of items_data) {
+
+    const item = item_data["Item"];
+    console.log(item);
 
     // Creates the div and assigns CSS class
     const itemDiv = document.createElement('div');
-    itemDiv.className = 'list-item'
+    itemDiv.className = 'list-item';
 
     // Creates a checkbox
-    const itemCheckbox = document.createElement('input')
-    itemCheckbox.type = "checkbox"
-    itemCheckbox.id = `checkbox ${item.item_id}`
+    const itemCheckbox = document.createElement('input');
+    itemCheckbox.type = "checkbox";
+    itemCheckbox.id = `checkbox ${item.item_id}`;
     itemCheckbox.checked = item.is_checked;
-    // --- NEW LOGIC: Listen for the user clicking the checkbox ---
+
+    // Update databse when checkbox is toggled 
     itemCheckbox.addEventListener('change', async (event) => {
       // Grabs current state of checkbox
       const newCheckedState = event.target.checked;
@@ -163,7 +262,7 @@ function createListUI(data) {
       const { error } = await supabase
         .from('Item')
         .update({ is_checked: newCheckedState })
-        .eq('item_id', item.item_id); // The SQL 'WHERE' clause
+        .eq('item_id', item.item_id); // Basically a SQL 'WHERE' clause
 
       // On fail, send log and flip checkbox back to original state
       if (error) {
@@ -173,7 +272,6 @@ function createListUI(data) {
         console.log(`Successfully updated ${item.item_name} to ${newCheckedState}`);
       }
     });
-    // ------------------------------------------------------------
 
     // Creates the name text
     const nameParagraph = document.createElement('p');
@@ -184,7 +282,23 @@ function createListUI(data) {
     itemDiv.appendChild(itemCheckbox);
     itemDiv.appendChild(nameParagraph);
     itemListElement.appendChild(itemDiv);
-  };
+  }
+
+  // Section for adding a new item to a list
+  const addItemDiv = document.createElement('div');
+  const addItemButton = document.createElement('button');
+  addItemButton.textContent = "Add Item";
+
+  itemListElement.appendChild(addItemDiv);
+  addItemDiv.appendChild(addItemButton);
+
+  addItemButton.addEventListener('click', async () => {
+    console.log("Adding item to", list_name);
+    writeItem(list_id, "test_item_in_list", 1);
+    // TODO: add a field so that you can add named items
+  });
+
 
 }
+// -------------------------------------------------------------------
 
